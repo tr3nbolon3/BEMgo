424a3b1e6a00ff270f8af71b8c92c8d71465a7b5
Konstantin Zemliakov <zemliakov.konstantin@gmail.com>
2018-01-03 17:33:56 +0600

    Fixed the script block.sh

--- block.sh
+++ block.sh
@@ -3,9 +3,9 @@ mkdir app/blocks/$1
 touch app/blocks/$1/$1.sass
 touch app/blocks/$1/$1.pug
 
-sed -i -e '1 s/^/include ..\/blocks\/'$1'\/'$1'\n/;' app/layouts/main.pug
-echo -e "@import '../blocks/$1/$1'" >> app/sass/main.sass
-echo -e "mixin $1()\n  div.$1&attributes(attributes)" >> app/blocks/$1/$1.pug
-echo -e ".$1\n  " >> app/blocks/$1/$1.sass
+sed -i '1 s/^/include ..\/blocks\/'$1'\/'$1'\n/;' app/layouts/main.pug
+echo "@import '../blocks/$1/$1'" >> app/sass/main.sass
+echo "mixin $1()\n  div.$1&attributes(attributes)" >> app/blocks/$1/$1.pug
+echo ".$1\n  " >> app/blocks/$1/$1.sass
 
 echo "Блок $1 создан"
